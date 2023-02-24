4f6e8487db1bed5d7a92dc55cded37a546b8a3c1
Paulo Pinto <gomescasador@gmail.com>
2023-02-24 22:39:57 +0000

    Adicionado mostrar/esconde em todos os locais de pistas e ordenado alfabetimente

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,8 +1,8 @@
+
+
 function consultar(posicao) {
     let pistaBox = document.getElementsByClassName('pistas-box')
     let textAreaPistaBox = pistaBox[posicao].childNodes[3]
 
     console.log(textAreaPistaBox.classList.toggle('textarea-none'))
-
-
 }
