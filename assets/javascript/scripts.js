3278f757b4d1c8a2ede26389292806038d81d865
Paulo Pinto <gomescasador@gmail.com>
2023-02-25 17:57:06 +0000

    Ajustes finos no layout

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,8 +1,6 @@
-
-
 function consultar(posicao, classeBox) {
     let pistaBox = document.getElementsByClassName(classeBox)
     let textAreaPistaBox = pistaBox[posicao].childNodes[3]
 
-    console.log(textAreaPistaBox.classList.toggle('textarea-none'))
+    textAreaPistaBox.classList.toggle('textarea-none')
 }
